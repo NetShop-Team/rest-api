0fe8325d305923916c632613bd42520a93fde619
Sayat <sayatumarov1@gmail.com>
2021-08-10 16:52:16 +0600

    lkjxdnvlkjdfv

--- getUsers/getUsers.go
+++ getUsers/getUsers.go
@@ -8,36 +8,36 @@ package getUsers
 	"fmt"
 )
 
-// type User struct {
-// 	Id   int
-// 	Login string
-// 	Password string
-// }
-// var db *sql.DB
-// var err error
+type User struct {
+	Id   int
+	Login string
+	Password string
+}
+var db *sql.DB
+var err error
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
-	// db, err = sql.Open("mysql", "RightyDev:NetShop2021@tcp(127.0.0.1:3306)/backend")
-	// if err != nil {
-	// 	panic(err.Error())
-	// }
-	// defer db.Close()
-	// w.Header().Set("Content-Type", "application/json")
-	// var users []User
-	// result, err := db.Query("SELECT id, login, password from users")
-	// if err != nil {
-	// 	panic(err.Error())
-	// }
-	// defer result.Close()
+	db, err = sql.Open("mysql", "RightyDev:NetShop2021@tcp(127.0.0.1:3306)/backend")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	w.Header().Set("Content-Type", "application/json")
+	var users []User
+	result, err := db.Query("SELECT id, login, password from users")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
 
-	// for result.Next() {
-	// 	var user User
-	// 	er := result.Scan(&user.Id, &user.Login, &user.Password)
-	// 	if er != nil {
-	// 		panic(er.Error())
-	// 	}
-	// 	users = append(users, user)
-	// }
-	// json.NewEncoder(w).Encode(users)
+	for result.Next() {
+		var user User
+		er := result.Scan(&user.Id, &user.Login, &user.Password)
+		if er != nil {
+			panic(er.Error())
+		}
+		users = append(users, user)
+	}
+	json.NewEncoder(w).Encode(users)
 	fmt.Fprintf(w, "qwertty")
 }
