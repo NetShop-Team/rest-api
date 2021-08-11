5124fb7ef261535d6f552b51c7e81939258b1907
Sayat <sayatumarov1@gmail.com>
2021-08-11 14:01:04 +0600

    jndcasn

--- getUsers/getUser.go
+++ getUsers/getUser.go
@@ -1,35 +1,35 @@
-// package getUsers
+package getUsers
 
-// import (
-// 	"database/sql"
-// 	"encoding/json"
-// 	"net/http"
-// 	"github.com/gorilla/mux"
-// 	_ "github.com/go-sql-driver/mysql"
-// )
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+	"github.com/gorilla/mux"
+	_ "github.com/go-sql-driver/mysql"
+)
 
 
-// func GetUser(w http.ResponseWriter, r *http.Request) {
+func GetUser(w http.ResponseWriter, r *http.Request) {
 
-// 	db, err = sql.Open("mysql", "RightyDev:NetShop2021@tcp(localhost:3306)/backend")
-// 	if err != nil {
-// 		panic(err.Error())
-// 	}
-// 	defer db.Close()
+	db, err = sql.Open("mysql", "RightyDev:NetShop2021@tcp(localhost:3306)/backend")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
 
-// 	w.Header().Set("Content-Type", "application/json")
-// 	params := mux.Vars(r)  //{id}
-// 	result, err := db.Query("SELECT id, login, password FROM users WHERE id = ?", params["id"])
-// 	if err != nil {
-// 	  panic(err.Error())
-// 	}  
-// 	defer result.Close()  
-// 	var user User  
-// 	for result.Next() {
-// 	  err := result.Scan(&user.Id, &user.Login, &user.Password)
-// 	  if err != nil {
-// 		panic(err.Error())
-// 	  }
-// 	}  
-// 	json.NewEncoder(w).Encode(user)
-//   }
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)  //{id}
+	result, err := db.Query("SELECT id, login, password FROM users WHERE id = ?", params["id"])
+	if err != nil {
+	  panic(err.Error())
+	}  
+	defer result.Close()  
+	var user User  
+	for result.Next() {
+	  err := result.Scan(&user.Id, &user.Login, &user.Password)
+	  if err != nil {
+		panic(err.Error())
+	  }
+	}  
+	json.NewEncoder(w).Encode(user)
+  }
